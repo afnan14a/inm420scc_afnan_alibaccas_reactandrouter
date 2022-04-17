8034141dfee62e84eee16363c2bc851c9fecc4d7
afnan14a <afnan.a@live.com>
2022-04-17 18:47:00 -0400

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+
+// pages
+import Home from './pages/home'
+import About from './pages/about'
+import Projects from './pages/projects'
+import projectDetails from './pages/projectDetails'
+import Offers from './pages/offers'
+import Contact from './pages/contact'
 
 function App() {
+  const [cartIsEmpty] =useState(false)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <BrowserRouter>
+      <nav>
+        <h1>Condominium Developments</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/offers">Offers</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/*" element={<About />} />
+        {/* <Route path="/projects/:id/" element={<projectDetails />} /> */}
+        <Route path="/projectsabc/*" element={<projectDetails />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+)
+}  
 
-export default App;
+
+
+export default App
